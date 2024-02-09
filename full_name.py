3185afb482654cafc22064e59833f09013709ed1
alefrp <alefrodrigopereira@yahoo.com.br>
2024-02-09 17:00:55 -0300

    first commit

---
+++ full_name.py
@@ -0,0 +1,16 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+# print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Languages: \n\tPython\n\tC\n\tJavaScript")
---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# print("Hello Python World!")
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+# Erro
+# message = "Hello Python Crash Course World!"
+# print(mesage)
+
+mesage = "Hello Python Crash Course World!"
+print(mesage)
