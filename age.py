8de55d764c0bbf86c613648e5fe19c51739167dd
alefrp <alefrodrigopereira@yahoo.com.br>
2024-02-16 09:04:23 -0300

    chapter: conditionals

---
+++ age.py
@@ -0,0 +1,11 @@
+age = 18
+print(age == 18)
+
+if age < 21:
+    print("You are not allowed to drink")
+elif age <= 21:
+    print("You are allowed to drink")
+elif age > 21:
+    print("You are not allowed to drink")
+else:
+    print("You are allowed to drink")
