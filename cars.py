a898a5bf37e5df23158521661b706d12f2424e95
alefrp <alefrodrigopereira@yahoo.com.br>
2024-02-16 07:57:45 -0300

    chapter: conditionals

--- cars.py
+++ cars.py
@@ -1,28 +1,44 @@
 # Sorting a list permanently with the sort() method.
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort()
-print(cars)
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
 
-# Sorting Descending
-cars.sort(reverse=True)
-print(cars)
+# Comparing values    
+print(cars[0] == 'bmw')
+car = 'audi'
+print(car == 'bmw')
 
-cars = ['bmw', 'audi', 'toyota', 'subaru']
-print("Here is the original list:")
-print(cars)
+# Letras maisculas e minisculas diferem
+car = 'Audi'
+print(car == 'audi') # False
+print(car.lower() == 'audi') # True
 
-# Sorting the list temporarily with the sorted() function.
-print("\nHere is the sorted list:")
-print(sorted(cars))
+# cars.sort()
+# print(cars)
 
-print("\nHere is the original list again:")
-print(cars)
+# # Sorting Descending
+# cars.sort(reverse=True)
+# print(cars)
 
-# Sorting a list in Reverse Order - last to first
-cars = ['bmw', 'audi', 'toyota', 'subaru']
-print(cars)
-cars.reverse()
-print(cars)
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print("Here is the original list:")
+# print(cars)
 
-# Getting list length
-print(len(cars))
+# # Sorting the list temporarily with the sorted() function.
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+
+# print("\nHere is the original list again:")
+# print(cars)
+
+# # Sorting a list in Reverse Order - last to first
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+# # Getting list length
+# print(len(cars))
