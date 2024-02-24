d55b7b6960210f33f753c68ae5faddb02e858a0b
AlefRP <93484657+AlefRP@users.noreply.github.com>
2024-02-24 17:10:02 -0300

    chapter function

--- formatted_name.py
+++ formatted_name.py
@@ -1,7 +1,21 @@
-def formatted_name(first_name, last_name):
+# def formatted_name(first_name, last_name):
+#     """Return a full name, neatly formatted."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+# musician = formatted_name('jimi', 'hendrix')
+# print(musician)
+
+def formatted_name(first_name, last_name, middle_name=''):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 musician = formatted_name('jimi', 'hendrix')
+print(musician)
+
+musician = formatted_name('john', 'hooker', 'lee')
 print(musician)
