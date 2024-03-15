0714a0b36e095d05a11115416cb1e39ae6f73b28
alefrp <alefrodrigopereira@yahoo.com.br>
2024-03-15 07:58:06 -0300

    feat: OOP

--- restaurant_v3.py
+++ restaurant_v3.py
@@ -1,7 +1,8 @@
 class Restaurant:
-    def __init__(self, name, cuisine_type):
+    def __init__(self, name, cuisine_type, number_served = 0):
         self.name = name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
         
     def describe_restaurant(self):
         """
@@ -15,6 +16,19 @@ class Restaurant:
         """
         print(f"{self.name} is now open!")
         
+    def set_number_served(self, number_served):
+        """
+        Set the number of customers that have been served.
+        """
+        self.number_served = number_served
+        
+    def increment_number_served(self, increment):
+        """
+        Increment the number of customers that have been served by a given amount.
+        """
+        self.number_served += increment
+        print(f"The number of customers served is now {self.number_served}")
+        
 # restaurant = Restaurant("The Pizza Palace", "Pizza")
 
 # print(restaurant.name)
@@ -23,11 +37,20 @@ class Restaurant:
 # restaurant.describe_restaurant()
 # restaurant.open_restaurant()
 
-restaurant_1 = Restaurant("The Kebab Palace", "Kebab")
-restaurant_2 = Restaurant("The Burger Palace", "Burger")
-restaurant_3 = Restaurant("The Fish Palace", "Fish")
+# restaurant_1 = Restaurant("The Kebab Palace", "Kebab")
+# restaurant_2 = Restaurant("The Burger Palace", "Burger")
+# restaurant_3 = Restaurant("The Fish Palace", "Fish")
 
-restaurant_1.describe_restaurant()
-restaurant_2.describe_restaurant()
-restaurant_3.describe_restaurant()
-        
+# restaurant_1.describe_restaurant()
+# restaurant_2.describe_restaurant()
+# restaurant_3.describe_restaurant()
+
+restaurant = Restaurant("The Pizza Palace", "Pizza")
+
+print(restaurant.number_served)
+# restaurant.number_served = 10
+restaurant.set_number_served(10)
+
+print(restaurant.number_served)
+
+restaurant.increment_number_served(5)
--- user_v3.py
+++ user_v3.py
@@ -1,13 +1,17 @@
 class User:
-    def __init__(self, first_name, last_name, age, gender, email):
+    def __init__(self, first_name, last_name, age, gender, email, loggin_attempts=0):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.gender = gender
         self.email = email
-        self.login_attempts = 0
+        self.login_attempts = loggin_attempts
         
     def describe_user(self):
+        """
+        A method to describe the user's first name, last name, age, gender, and email.
+        """
+        """"""
         print(f"First name: {self.first_name}")
         print(f"Last name: {self.last_name}")
         print(f"Age: {self.age}")
@@ -15,7 +19,17 @@ class User:
         print(f"Email: {self.email}")
         
     def greet_user(self):
+        """_summary_
+        """
         print(f"Hello {self.first_name} {self.last_name}!")
+        
+    def increment_loggin_attempts(self):
+        self.login_attempts += 1
+        print(f"Login attempts: {self.login_attempts}")
+        
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Login attempts: {self.login_attempts}")
         
 first_user = User("John", "Doe", 30, "male", "john.doe@example.com")
 
@@ -26,3 +40,8 @@ second_user = User("Jane", "Doe", 25, "female", "jane.doe@example.com")
 
 second_user.describe_user()
 second_user.greet_user()
+
+for _ in range(3):
+    second_user.increment_loggin_attempts()
+
+second_user.reset_login_attempts()
