cb71277308a84f416f2e00e5bcb5310aab8b7e95
alefrp <alefrodrigopereira@yahoo.com.br>
2024-03-25 13:20:18 -0300

    feat: OOP

--- eletric_car.py
+++ eletric_car.py
@@ -26,8 +26,29 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to the odometer reading."""
         self.odometer_reading += miles
-    
+        
+    def fill_gas_tank(self):
+        """Fill the gas tank."""
+        print("Filling the gas tank...")
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+        
+    def get_range(self):    
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 220
+        print(f"This car can go about {range} miles on a full charge.")
+        
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
@@ -36,12 +57,20 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 40
+        self.battery = Battery()
+        # self.battery_size = 40
     
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size."""
+    #     print(f"This car has a {self.battery_size}-kWh battery.")
+        
+    def fill_gas_tank(self): # Subscring to the parent method
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
         
 my_leaf = ElectricCar('tesla', 'leaf', 2019)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
+# my_leaf.describe_battery()
+my_leaf.battery.describe_battery() # Subscring to the child method
+my_leaf.battery.get_range() # Subscring to the child method
+my_leaf.fill_gas_tank()
