a0add02981316c426dffbe83f2176a09981c3c40
alefrp <alefrodrigopereira@yahoo.com.br>
2024-05-03 07:27:50 -0300

    feat: files and exceptions

--- file_reader.py
+++ file_reader.py
@@ -1,6 +1,19 @@
 from pathlib import Path
 
-path = Path("text_files/pi_digits.txt")
+path = Path("text_files\pi_million_digits.txt")
 contents = path.read_text().rstrip()
 
-print(contents)
+# print(contents)
+
+lines = contents.splitlines()
+
+# for line in lines:
+#     print(line.rstrip())
+
+pi_string = ''
+
+for line in lines:
+    pi_string += line.lstrip()
+    
+print(f"{pi_string[:52]}...") # print first 52 digits of pi_string
+print(len(pi_string))
